6ff0ea9068190c6a14c3662cdb0d3171b743bde5
Weslley <weslleyjonathas88@gmail.com>
2021-11-09 22:09:02 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,30 @@
+let form = document.getElementById('formulario');
+let pegarCep = document.getElementById('numero');
+let alerta = document.getElementById('alerta').style.display = 'none';
+
+form.addEventListener('submit', (event) => {
+    event.preventDefault();
+
+    var cep = pegarCep.value;
+    let url = `https://viacep.com.br/ws/${cep}/json/`
+
+    fetch(url)
+        .then((resposta) => resposta.json())
+        .then((data) => {
+            let endereco = data;
+
+            let enderecoCompleto =
+                `CEP: ${endereco.cep}, ${endereco.logradouro}, ${endereco.complemento},
+                Cidade: ${endereco.localidade}, Bairro: ${endereco.bairro}`;
+
+            if (endereco.erro != true) {
+                alert(enderecoCompleto)
+            }
+            else if (endereco.erro == true) {
+                alerta = document.getElementById('alerta').style.display = 'block';
+            }
+            document.getElementById("numero").value = "";
+        })
+        .catch((error) => console.log(error));
+
+});
